a9b26468f34b714647c97e5250896f6ad7293678
Dominik Marszałek <dominikmarszalek@Dominiks-MacBook-Pro.local>
2025-08-06 11:29:51 +0200

    Usuniecie starego projektu, zmiana struktury katalogow, dodanie podstawowego deploymentu

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import HeroBanner from "./components/HeroBaner";
+import AboutMe from "./components/AboutMe";
+import Colaboration from "./components/Colaboration";
+
+export default function Home() {
+  return (
+    <>
+      <HeroBanner />
+      <AboutMe />
+      <Colaboration />
+    </>
+  );
+}
