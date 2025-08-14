8f702b6cbaa6d898dd74a9cc5fed419058de1929
Dominik Marszałek <dominikmarszalek@Dominiks-MacBook-Pro.local>
2025-08-14 09:13:08 +0200

    Dodanie tekstu oraz dodanie linku

--- src/app/components/AboutMe.tsx
+++ src/app/components/AboutMe.tsx
@@ -1,8 +1,12 @@
+import Link from "next/link";
+
 export default function AboutMe() { 
     return (
         <section className="flex flex-col items-center justify-center py-7 px-6">
                 <h2 className="font-family-montserrat text-3xl mb-6">O mnie</h2>
-                <p className="text-center leading-[1.7]">Od 16 lat z wielkim zapałem zgłębiam tajniki pielęgnacji skóry, a retinoidy to mój numer jeden w kosmetyczce. Uważam, że piękna cera wymaga działań zarówno zewnętrznych, jak i wewnętrznych – dlatego stawiam na zdrową dietę oraz regularne treningi.</p>
+                <p className="text-center leading-[1.7] mb-4">Od 16 lat z ciekawością i zaangażowaniem zgłębiam, czym naprawdę jest skuteczna pielęgnacja skóry.</p>
+                <p className="text-center leading-[1.7] mb-4">Wierzę, że świadoma pielęgnacja to nie tylko sposób na wygląd, ale przede wszystkim wyraz troski o siebie i swoje samopoczucie.</p>
+                <p className="text-center leading-[1.7] mb-4">Dziś swoją wiedzę przekładam na praktykę, oferując <Link className="underline font-bold" href="/pielegnacje/spersonalizowany-plan-pielegnacyjny">indywidualne konsultacje pielęgnacyjne</Link>, które pomagają dobrać skuteczną, spersonalizowaną rutynę pielęgnacyjną, dostosowaną do Twoich potrzeb.</p>
         </section>
     )
 }
